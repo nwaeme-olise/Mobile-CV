2f2610cd4c65bb0bde7d1527d9f8a001d0b39f5e
Nwaeme Olisemeka <onwaeme@gmail.com>
2023-09-12 21:50:34 +0100

    Made adjustment to field list item

--- app/src/main/java/com/example/mobilecv/DisplayCvFragment.kt
+++ app/src/main/java/com/example/mobilecv/DisplayCvFragment.kt
@@ -36,7 +36,8 @@ class DisplayCvFragment : Fragment() {
             listOf(
                 FieldItem("Name", "Nwaeme Olisemeka"),
                 FieldItem("Slack Name", "O.Lee"),
-                FieldItem("GitHub Handle", "nwaeme-olise")
+                FieldItem("GitHub Handle", "nwaeme-olise"),
+                FieldItem("Bio", "Hi there! I’m Olise, a Software Engineer with a keen interest in developing native Android apps. I’m also a graduate of Computer Science from the University of Lagos.\\n Before starting my developer career, I worked as a Software Instructor, where I taught Android Development with Java, as well as Python and Web Development to 50+ students. I also taught programming to kids in primary and secondary schools using mini robotics kits.")
             )
         )
 
